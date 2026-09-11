69ea3cc063d1ab2a487482bbb4566a265568d87e
krishkrishna123 <krishna4925a@gmail.com>
2026-09-11 11:21:35 +0530

    Update firewall_services.sh

--- firewall_services.sh
+++ firewall_services.sh
@@ -3,40 +3,27 @@
 # ==========================================
 # Managing Firewall Services using firewalld
 #
-# Student Name:
-# Roll Number:
+# Student Name: KRISHNAKUMAR.R
+# Roll Number: 1U24IT058
 # ==========================================
 
-
 # Check current firewall services
-
-
-
+sudo firewall-cmd --list-services
 
 # Allow HTTP service
-
-
+sudo firewall-cmd --add-service=http
 
 # Allow HTTPS service
-
-
-
+sudo firewall-cmd --add-service=https
 
 # Display enabled services
-
-
-
+sudo firewall-cmd --list-services
 
 # Remove HTTP service
-
-
-
+sudo firewall-cmd --remove-service=http
 
 # Allow SSH permanently
-
-
-
+sudo firewall-cmd --permanent --add-service=ssh
 
 # Reload firewall configuration
-
-
+sudo firewall-cmd --reload
